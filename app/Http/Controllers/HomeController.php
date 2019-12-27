b4736a3dc6c8398675318f4a21d7e8e382900d03
Vlad Kyrychenko <vlad.kyr199@gmail.com>
2019-12-27 22:15:47 +0200

    feed fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function feed()
     {
         return view('feed');
     }
