1d00b38f137b0feb53b3b01387617fdcc68c191d
Vlad Kyrychenko <vlad.kyr199@gmail.com>
2019-12-28 04:53:41 +0200

    small fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,6 +14,6 @@ class Controller extends BaseController
     private $user;
 
     public function __construct() {
-        $this->user = auth()->user()->profile;
+        $this->user = auth()->user();
     }
 }
