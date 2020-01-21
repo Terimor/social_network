2bc125b054c316969a3b97d414c64f33f0442246
Vlad Kyrychenko <vlad.kyr199@gmail.com>
2020-01-21 10:53:52 +0200

    fixed link

--- resources/views/subscribes/subscribe.blade.php
+++ resources/views/subscribes/subscribe.blade.php
@@ -1,10 +1,10 @@
 <li>
     <div class="nearly-pepls">
         <figure>
-            <a href="{{ url('/profiles/'.$profile->user_id) }}" title=""><img src="{{ $profile->avatar }}" alt="avatar"></a>
+            <a href="{{ url('profile/'.$profile->user_id) }}" title=""><img src="{{ $profile->avatar }}" alt="avatar"></a>
         </figure>
         <div class="pepl-info">
-            <h4><a href="{{ url('/profiles/'.$profile->user_id) }}" title="">{{ $profile->name }}</a></h4>
+            <h4><a href="{{ url('profile/'.$profile->user_id) }}" title="">{{ $profile->name }}</a></h4>
             <span>ftv model</span>
             <a href="{{ $href }}" title="" class="add-butn action-button" data-ripple="">{{ $action }}</a>
         </div>
